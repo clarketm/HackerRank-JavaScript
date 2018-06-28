78a95dd5611cfad5fe197f66ceee4f2b2d54c989
Travis Clarke <travis.m.clarke@gmail.com>
2018-06-28 13:22:03 -0700

    add 2d array hourGlass solution

---
+++ hourGlass.js
@@ -0,0 +1,45 @@
+/**
+ *
+ * Hourglass Sum
+ *
+ * @param {number[]} arr
+ * @return {number}
+ */
+
+function hourglassSum(arr) {
+  let max = -Number.MAX_VALUE;
+
+  for (let i = 0; i < arr.length - 2; i++) {
+    for (let j = 0; j < arr[i].length - 2; j++) {
+      let sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+      max = Math.max(sum, max);
+    }
+  }
+
+  return max;
+}
+
+// Test
+if (require.main === module) {
+  // console.log(hourglassSum([
+  //   [1, 1, 1, 0, 0, 0],
+  //   [0, 1, 0, 0, 0, 0],
+  //   [1, 1, 1, 0, 0, 0],
+  //   [0, 9, 2, -4, -4, 0],
+  //   [0, 0, 0, -2, 0, 0],
+  //   [0, 0, -1, -2, -4, 0]
+  // ])); // 13
+
+  console.log(hourglassSum([
+    [-1, 1, -1, 0, 0, 0],
+    [0, -1, 0, 0, 0, 0],
+    [-1, -1, -1, 0, 0, 0],
+    [0, -9, 2, -4, -4, 0],
+    [-7, 0, 0, -2, 0, 0],
+    [0, 0, -1, -2, -4, 0]
+  ]))
+}
+
+module.exports = {
+  hourglassSum
+};
